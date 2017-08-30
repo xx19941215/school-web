66249ed038ae2de455c82200894749aa6083a944
xiao <xx19931215@gmail.com>
2017-08-30 20:48:43 +0800

    add:random

--- app/Helper/functions.php
+++ app/Helper/functions.php
@@ -10,3 +10,24 @@ function prop($arr, $key, $default = '')
 {
     return $arr[$key] ?? $default;
 }
+
+function transName($schoolName = '')
+{
+    if ($schoolName == '') return '';
+
+    if (strpos($schoolName, '市第')) {
+        $schoolName = preg_replace('/市第/', '', $schoolName);
+        $schoolName = preg_replace('/中学/', '中', $schoolName);
+
+        return $schoolName;
+    }
+
+    if (strpos($schoolName, '县第')) {
+        $schoolName = preg_replace('/县第/', '', $schoolName);
+        $schoolName = preg_replace('/中学/', '中', $schoolName);
+
+        return $schoolName;
+    }
+
+    return $schoolName;
+}
